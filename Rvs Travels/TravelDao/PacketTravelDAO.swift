fe38e94db1b0c4e6fa947b55a2be6279789b6984
Renato <renato.vieira@stant.com.br>
2021-03-24 22:14:56 -0300

    Changed name attributes packet travels

--- Rvs Travels/TravelDao/PacketTravelDAO.swift
+++ Rvs Travels/TravelDao/PacketTravelDAO.swift
@@ -12,7 +12,7 @@ class PacketTravelDAO: NSObject {
     
     func returnAllTravels() -> Array<PacketTravel> {
         
-        let pacotePortoGalinhas = PacketTravel(nameHotel:         "Resort Porto de Galinhas",
+        let packetPortoGalinhas = PacketTravel(nameHotel:         "Resort Porto de Galinhas",
                                                descriptionTravel: "Hotel + café da manhã",
                                                dateTravel:        "8~15 de agosto",
                                                travel:            Travel(title:        "Porto de Galinhas",
@@ -20,7 +20,7 @@ class PacketTravelDAO: NSObject {
                                                                          price:        2490.99,
                                                                          wayImage:     "img6.jpg"))
         
-        let pacoteBuzios = PacketTravel(nameHotel:         "Resort Buzios Spa",
+        let packetBuzios = PacketTravel(nameHotel:         "Resort Buzios Spa",
                                         descriptionTravel: "Hotel + café da manhã",
                                         dateTravel:        "9~16 de setembro",
                                         travel:            Travel(title:        "Buzios",
@@ -28,7 +28,7 @@ class PacketTravelDAO: NSObject {
                                                                   price:        1990.99,
                                                                   wayImage:     "img7.jpg"))
         
-        let pacoteNatal = PacketTravel(nameHotel:         "Resort Natal Show",
+        let packetNatal = PacketTravel(nameHotel:         "Resort Natal Show",
                                        descriptionTravel: "Hotel + café da manhã",
                                        dateTravel:        "13~18 de setembroo",
                                        travel:            Travel(title:        "Natal",
@@ -36,7 +36,7 @@ class PacketTravelDAO: NSObject {
                                                                  price:        1700.99,
                                                                  wayImage:     "img8.jpg"))
         
-        let pacoteRioDeJaneiro = PacketTravel(nameHotel:         "Resort RJ Spa",
+        let packetRioDeJaneiro = PacketTravel(nameHotel:         "Resort RJ Spa",
                                               descriptionTravel: "Hotel + café da manhã",
                                               dateTravel:        "9~13 de outubro",
                                               travel:            Travel(title:        "Rio de Janeiro",
@@ -44,7 +44,7 @@ class PacketTravelDAO: NSObject {
                                                                         price:        2300.99,
                                                                         wayImage:     "img9.jpg"))
         
-        let pacoteAmazonas = PacketTravel(nameHotel:         "Pousada Amazonas Plus",
+        let packetAmazonas = PacketTravel(nameHotel:         "Pousada Amazonas Plus",
                                           descriptionTravel: "Hotel + café da manhã",
                                           dateTravel:        "9~13 de outubro",
                                           travel:            Travel(title:        "Amazonas",
@@ -52,7 +52,7 @@ class PacketTravelDAO: NSObject {
                                                                     price:        2850.99,
                                                                     wayImage:     "img10.jpg"))
         
-        let pacoteSalvador = PacketTravel(nameHotel:         "Pousada Salvador",
+        let packetSalvador = PacketTravel(nameHotel:         "Pousada Salvador",
                                           descriptionTravel: "Hotel + café da manhã",
                                           dateTravel:        "5~10 de novembro",
                                           travel:            Travel(title:        "Salvador",
@@ -60,7 +60,7 @@ class PacketTravelDAO: NSObject {
                                                                     price:        1880.99,
                                                                     wayImage:     "img11.jpg"))
         
-        let listPackets: Array <PacketTravel> = [pacotePortoGalinhas, pacoteBuzios, pacoteNatal, pacoteRioDeJaneiro, pacoteAmazonas, pacoteSalvador]
+        let listPackets: Array <PacketTravel> = [packetPortoGalinhas, packetBuzios, packetNatal, packetRioDeJaneiro, packetAmazonas, packetSalvador]
         
         return listPackets
     }
